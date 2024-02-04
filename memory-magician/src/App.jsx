7745cde1071f0e944b9282caeb37b5afb5592040
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-02-04 12:33:36 -0800

    a good example of race condition

--- memory-magician/src/App.jsx
+++ memory-magician/src/App.jsx
@@ -1,9 +1,9 @@
 import { generateClient } from 'aws-amplify/api';
+import { useState, useRef, useEffect } from 'react';
+import { getUserByEmail, createUserAPI } from './utilities/apis/userAPI'
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-const initialState = { name: '', description: '' };
-const client = generateClient();
-
+import { fetchUserAttributes, updateUserAttribute } from 'aws-amplify/auth';
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Navigate, Link, useNavigate } from 'react-router-dom';
@@ -15,15 +15,42 @@ import TodayReviewScreen from './screens/todayreview';
 import HomeScreen from './screens/home';
 import NavBar from './components/NavigationBar';
 
+const initialState = { name: '', description: '' };
+const client = generateClient();
+
+
 const App = ({ signOut, user }) => {
 
   const navigate = useNavigate();
-
+  useEffect(() => {
+    addUserToDatabaseWhenFirstSignIn()
+  }, []);
   const handleNavigation = (path) => {
     // ... submit logic ...
     navigate(path);
   };
-  
+
+  const addUserToDatabaseWhenFirstSignIn = async () => {
+    try {
+      const currentUser = await fetchUserAttributes()
+      const r = await getUserByEmail(currentUser["email"])
+      console.log(r)
+      if (r.length === 0) {
+          const userData = {
+            email: currentUser["email"],
+            name: "shen"
+          };
+          console.log("here2")
+          await createUserAPI(userData);
+          console.log("here4")
+      } else {
+        console.log("User is already in the database")
+      }
+    } catch (error) {
+        console.error("Error during addUserToDatabaseWhenFirstSignIn: ", error);
+        throw error;
+    }
+  }
   return (
     <div>
       <NavBar /> {/* NavBar is always displayed */}
