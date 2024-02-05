176145675c5153f56c5be917a857d126e9c4fa3f
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-02-05 12:47:30 -0800

    added context sharing state to memory calendar and set time zone to utc as default

--- memory-magician/src/components/MemoryCalendar.jsx
+++ memory-magician/src/components/MemoryCalendar.jsx
@@ -7,10 +7,12 @@ import { useState, useEffect } from "react";
 import "../css/MemoryCalendar.css";
 import { getAllUnreviewedCardsOfUser } from '../utilities/apis/carduserAPI';
 import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useMemory } from "../context/MemoryContext.jsx"
 
 const localizer = momentLocalizer(moment);
 
 function MemoryCalendar(props) {
+  const { memoryAdded } = useMemory();
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -23,10 +25,10 @@ function MemoryCalendar(props) {
             formattedEvents.push({
               title: cardUser.card.content,
               start: new Date(
-                moment(cardUser.reviewDate).tz("America/Los_Angeles").toDate()
+                moment(cardUser.reviewDate).utc().toDate()
               ),
               end: new Date(
-                moment(cardUser.reviewDate).tz("America/Los_Angeles").toDate()
+                moment(cardUser.reviewDate).utc().toDate()
               ),
               color: "#c5b4e3"
               // cardUser.title === "Special Event"
@@ -41,7 +43,10 @@ function MemoryCalendar(props) {
       }
     }
     fetchCards()
-  }, []);
+    if (memoryAdded) {
+      fetchCards(); // call it when a new card is created
+    }    
+  }, [memoryAdded]);
 
   return (
     <div style={props.style}>
