58267f69b75856d8ed848ebdbc6f1167339e7ce1
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-02-06 19:26:58 -0800

    added a footer for copyright and corresponding styles

---
+++ memory-magician/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>{new Date().getFullYear()} &copy; Memory Mate. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
