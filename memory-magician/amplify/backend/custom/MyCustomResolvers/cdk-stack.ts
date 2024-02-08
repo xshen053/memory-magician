fa887cc1b23640c573d7033c61448aa8d96b49ea
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-02-07 19:41:19 -0800

    no error warning resolver

---
+++ memory-magician/amplify/backend/custom/MyCustomResolvers/cdk-stack.ts
@@ -0,0 +1,105 @@
+// import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Fn, Stack, StackProps, CfnParameter, aws_appsync as appsync } from 'aws-cdk-lib';                 // core constructs
+import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
+import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
+
+//import * as iam from 'aws-cdk-lib/aws-iam';
+//import * as sns from 'aws-cdk-lib/aws-sns';
+//import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+//import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const requestVTL = `
+<YOUR CUSTOM VTL REQUEST MAPPING TEMPLATE HERE>
+`;
+const responseVTL = `
+<YOUR CUSTOM VTL RESPONSE MAPPING TEMPLATE HERE>
+`;
+
+export class cdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+    super(scope, id, props);
+    /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
+    new CfnParameter(this, 'env', {
+      type: 'String',
+      description: 'Current Amplify CLI env name',
+    });
+
+    const retVal:AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this, 
+      amplifyResourceProps.category, 
+      amplifyResourceProps.resourceName, 
+      [{
+        category: "api",
+        resourceName: "<YOUR-API-NAME>"
+      }]
+    );
+
+    /* AWS CDK code goes here - learn more: https://docs.aws.amazon.com/cdk/latest/guide/home.html */
+    
+    // Example 1: Set up an SQS queue with an SNS topic 
+
+    /*
+    const amplifyProjectInfo = AmplifyHelpers.getProjectInfo();
+    const sqsQueueResourceNamePrefix = `sqs-queue-${amplifyProjectInfo.projectName}`;
+    const queue = new sqs.Queue(this, 'sqs-queue', {
+      queueName: `${sqsQueueResourceNamePrefix}-${cdk.Fn.ref('env')}`
+    });
+    // 👇create sns topic
+    
+    const snsTopicResourceNamePrefix = `sns-topic-${amplifyProjectInfo.projectName}`;
+    const topic = new sns.Topic(this, 'sns-topic', {
+      topicName: `${snsTopicResourceNamePrefix}-${cdk.Fn.ref('env')}`
+    });
+    // 👇 subscribe queue to topic
+    topic.addSubscription(new subs.SqsSubscription(queue));
+    new cdk.CfnOutput(this, 'snsTopicArn', {
+      value: topic.topicArn,
+      description: 'The arn of the SNS topic',
+    });
+    */
+
+    // Example 2: Adding IAM role to the custom stack 
+    /*
+    const roleResourceNamePrefix = `CustomRole-${amplifyProjectInfo.projectName}`;
+    
+    const role = new iam.Role(this, 'CustomRole', {
+      assumedBy: new iam.AccountRootPrincipal(),
+      roleName: `${roleResourceNamePrefix}-${cdk.Fn.ref('env')}`
+    }); 
+    */
+
+    // Example 3: Adding policy to the IAM role
+    /*
+    role.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['*'],
+        resources: [topic.topicArn],
+      }),
+    );
+    */
+
+    // Access other Amplify Resources 
+    /*
+    const retVal:AmplifyDependentResourcesAttributes = AmplifyHelpers.addResourceDependency(this, 
+      amplifyResourceProps.category, 
+      amplifyResourceProps.resourceName, 
+      [
+        {category: <insert-amplify-category>, resourceName: <insert-amplify-resourcename>},
+      ]
+    );
+    */
+    const resolver = new appsync.CfnResolver(this, 'custom-resolver', {
+      // apiId: retVal.api.new.GraphQLAPIIdOutput,
+      // https://github.com/aws-amplify/amplify-cli/issues/9391#event-5843293887
+      // If you use Amplify you can access the parameter via Ref since it's a CDK parameter passed from the root stack.
+      // Previously the ApiId is the variable Name which is wrong , it should be variable value as below
+      apiId: Fn.ref(retVal.api.memorymagician.GraphQLAPIIdOutput),
+      fieldName: 'querySomething',
+      typeName: 'Mutation', // Query | Mutation | Subscription
+      requestMappingTemplate: requestVTL,
+      responseMappingTemplate: responseVTL,
+      dataSourceName: 'UserCardsTable' // DataSource name
+    });
+    
+  }
+}
