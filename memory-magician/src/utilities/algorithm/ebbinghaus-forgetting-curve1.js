f215634ec2feb325a33f4897c2a03005205d1672
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-02-17 10:11:58 -0800

    fix a bug in generateAllReviewDates, change get/setDate to get/setUtcDate

--- memory-magician/src/utilities/algorithm/ebbinghaus-forgetting-curve1.js
+++ memory-magician/src/utilities/algorithm/ebbinghaus-forgetting-curve1.js
@@ -1,16 +1,16 @@
 
 /**
  * Generates all review dates based on the provided start date and intervals.
- * @param {string} todayDate - utc format date
+ * @param {string} todayDateString - utc format date string
  * e.g. 2024-02-04T22:15:52.019Z
  * @param {number[]} [intervals=[0, 1, 5, 10, 20, 30, 45, 60, 90, 120, 150]] 
  *  - An array of intervals (in days) for review dates. If not provided, defaults to a predefined set of intervals.
  * @returns {string[]} An array of review dates in ISO string format.
  */
-export const generateAllReviewDates = (todayDate, intervals = [0, 1, 5, 10, 20, 30, 45, 60, 90, 120, 150]) => {
+export const generateAllReviewDates = (todayDateString, intervals = [0, 1, 5, 10, 20, 30, 45, 60, 90, 120, 150]) => {
   return intervals.map(interval => {
-    let date = new Date(todayDate); // Create a new Date object for each interval
-    date.setDate(date.getDate() + interval); // Add the interval to the new date
+    let date = new Date(todayDateString); // Create a new Date object for each interval
+    date.setUTCDate(date.getUTCDate() + interval); // Add the interval to the new date
     return date.toISOString(); // Convert to ISO string format
   });
 }
