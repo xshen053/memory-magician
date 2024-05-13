9b3011af3e49fd97dd1d785daf5b6c5c6bdb5928
Xiaxi Shen <shenxiaxi26@gmail.com>
2024-05-13 10:02:49 -0700

    Change @auth from public to owner, now data is no longer publicly available

--- memory-magician/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ memory-magician/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -2,8 +2,7 @@ export type AmplifyDependentResourcesAttributes = {
   "api": {
     "memorymagician": {
       "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
+      "GraphQLAPIIdOutput": "string"
     }
   },
   "auth": {
